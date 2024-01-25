1204958bbf17e88d426bab1c0afa47289bb352aa
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-01-25 23:48:41 +0900

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="panel panelOne">
+        <h1>hello.</h1>
+      </div>
+      <div className="panel"></div>
+      <div className="panel panelTwo">
+      <h1>hello.</h1>
+      </div>
+      <div className="panel"></div>
+      <div className="panel panelThree">
+        <div className="container">
+          <nav>
+            <h3>SM</h3>
+            <h3>WORKS</h3>
+            <h3>ABOUT</h3>
+          </nav>
+          <h1>hello</h1>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
